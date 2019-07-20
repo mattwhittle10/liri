0c5c663812eabaceaa8109bc0719988578647cf5
Whittle <matt.whittle10@gmail.com>
2019-07-20 16:59:19 -0600

    spotify request working. Currently getting the concert request added

--- liri.js
+++ liri.js
@@ -1,50 +1,55 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
-// var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify({
+    id: 'a296ce6a2f9f4d82bc0a18bdef3fe7a0',
+    secret: '81ea763416b344d092c2a840e261a379'
+  });
+
+
 var inquirer = require("inquirer");
 
-var movieName = "shrek";
 
-axios.get("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy").then(
-    function(response) {
-        console.log("Movie Title: " + response.data.Title);
-        console.log("Imdb rating is: " + response.data.imdbRating);
-        console.log("Release Year: " + response.data.Year);
-        console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-        console.log("Country it was produce in: " + response.data.Country);
-        console.log("Language: " + response.data.Language);
-        console.log("Plot: " + response.data.Plot)
-        console.log("Actors: " + response.data.Actors);
-        
-    })
-    .catch(function(error){
-    if (error.response){
-        console.log("-----------Data-----------");
-        console.log(error.response.data);
-        console.log("-----------Status---------");
-        console.log(eroor.response.status);
-        console.log("-----------Status---------");
-        console.log(error.response.headers);
-    } else if (error.request) {
-        console.log(error.request);
-    } else {
-        console.log("Error", error.message);
-    }
-    console.log(error.config);
-});
+var search = (process.argv[2]);
+var userSearch = (process.argv.slice(3).join(" "));
 
-// inquirer.prompt([
-//     {
-//         type: "input",
-//         message: "Enter a movie title: ",
-//         name: "title"
-//     }
-// ]).then(function(movie){
-//     movieName.push(inquirerResponse.name);
-//     console.log(movieName);
-// });
-
-// var movie = parseFloat(process.argv[2]);
-// var number1 = parseFloat(process.argv[3]);
-console.log("hello world");
+switch(search) {
+    case "movie-this":
+            axios.get("http://www.omdbapi.com/?t=" + userSearch + "&y=&plot=short&apikey=trilogy").then(
+                function(response) {
+                    console.log("Movie Title: " + response.data.Title);
+                    console.log("Imdb rating is: " + response.data.imdbRating);
+                    console.log("Release Year: " + response.data.Year);
+                    console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+                    console.log("Country it was produce in: " + response.data.Country);
+                    console.log("Language: " + response.data.Language);
+                    console.log("Plot: " + response.data.Plot)
+                    console.log("Actors: " + response.data.Actors);    
+                });
+    break;
+    case "spotify-this-song":
+            spotify.search(
+                { type: 'track', 
+                query: userSearch
+                }, function(err, data) {
+                if (err) {
+                  return console.log('Error occurred: ' + err);
+                }
+              console.log("Song Title: " + data.tracks.items[0].artists[0].name);
+              console.log("Artist: " + data.tracks.items[0].name);
+              console.log("Link to Track: " + data.tracks.items[0].external_urls.spotify);
+              console.log("Album: " + data.tracks.items[0].album.name)
+              });
+    break;
+    case "concert-this":
+            axios.get("https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp").then(function(response){
+                console.log(response.data[0].venue.name);
+                console.log(response.data[0].venue.city + ", " + response.data[0].venue.region + " " + response.data[0].venue.country);
+                console.log(response.data[0]);
+            });
+    break;
+    default:
+        console.log("Type movie, song, or band to start your search");
+}
